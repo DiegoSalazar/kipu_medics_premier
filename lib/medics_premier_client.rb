b5a170e83de3962b3a8c1f49b7de87af205a2cb9
Diego Salazar <diego@greyrobot.com>
2017-07-28 17:03:48 -0400

    added rspec, and starter code

--- lib/medics_premier_client.rb
+++ lib/medics_premier_client.rb
@@ -1,5 +1,11 @@
 require "medics_premier_client/version"
 
 module MedicsPremierClient
-  # Your code goes here...
+  ENDPOINT = ENV.fetch 'KIPU_MEDICS_PREMIER_ENDPOINT', 'https://api.myadsc.com'
+  REQUEST_URI = ENV.fetch 'KIPU_MEDICS_PREMIER_REQUEST_URI', '/api/v4/edi'
+  SECRET_KEY = ENV.fetch 'KIPU_MEDICS_PREMIER_SECRET_KEY', 'O3iO1odLol0N8m5yOWBmveSDTJC2sYbE'
+
+  # NOTE: They don't use an ACCESS_ID
+
+
 end
---
+++ spec/medics_premier_client_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require 'medics_premier_client'
+
+describe MedicsPremierClient do
+
+end
