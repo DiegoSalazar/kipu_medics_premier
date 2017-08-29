8ecd69e270ab57e7a145569d7933fafee6337ee0
Diego Salazar <diego@greyrobot.com>
2017-08-29 13:43:36 -0400

    removed requirement for creds ENV vars"

--- lib/medics_premier.rb
+++ lib/medics_premier.rb
@@ -9,18 +9,10 @@ require 'uri'
 
 # Namespace and API Client factory
 module MedicsPremier
-  MissingConfig = Class.new StandardError
-
   ENDPOINT = ENV.fetch 'KIPU_MEDICS_PREMIER_ENDPOINT', 'https://api.myadsc.com'
   REQUEST_URI = ENV.fetch 'KIPU_MEDICS_PREMIER_REQUEST_URI', '/api/v4/edi'
-
-  GATEWAY_KEY = ENV.fetch 'KIPU_MEDICS_PREMIER_GATEWAY_KEY' do
-    raise MissingConfig, 'Missing KIPU_MEDICS_PREMIER_GATEWAY_KEY env var'
-  end
-
-  SECRET_KEY = ENV.fetch 'KIPU_MEDICS_PREMIER_SECRET_KEY' do
-    raise MissingConfig, 'Missing KIPU_MEDICS_PREMIER_SECRET_KEY env var'
-  end
+  GATEWAY_KEY = ENV['KIPU_MEDICS_PREMIER_GATEWAY_KEY']
+  SECRET_KEY = ENV['KIPU_MEDICS_PREMIER_SECRET_KEY']
 
   # Instantiates a new client instance with default config
   def self.client(endpoint: ENDPOINT, request_uri: REQUEST_URI, gateway_key: GATEWAY_KEY, secret_key: SECRET_KEY)
